b1cf5730cbececda44989343c69075ce73dc9fb4
ILCart <143832706+ILCart@users.noreply.github.com>
2024-01-20 19:27:22 -0800

    day 2 start

---
+++ Day2/part1.js
@@ -0,0 +1,3 @@
+const input = `Game 1: 7 blue, 5 red; 10 red, 7 blue; 5 blue, 4 green, 15 red; 4 green, 6 red, 7 blue; 5 green, 8 blue, 4 red; 5 red, 4 blue, 3 green`
+
+console.log(input.split(/: |; /g))
