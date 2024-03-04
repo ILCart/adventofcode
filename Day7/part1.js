bc75387f6f2e3e6c941fba5a41c96ff373cba318
ILCart <143832706+ILCart@users.noreply.github.com>
2024-03-03 22:05:48 -0800

    Update part1.js

--- Day7/part1.js
+++ Day7/part1.js
@@ -1,5 +1,5 @@
-const input = 
-`32T3K 765
+const input =
+    `32T3K 765
 T55J5 684
 KK677 28
 KTJJT 220
@@ -8,46 +8,59 @@ QQQJA 483`
 
 let cards = "AKQJT98765432";
 
-let e = input.split("\n").map(x=>x.split(" "));
+let e = input.split("\n").map(x => x.split(" "));
 
 let hands = {}
-for(const cardset of e){
+for (const cardset of e) {
     let hand = cardset[0];
     let repeats = {};
     for (let i = 0, len = hand.length; i < len; i++) {
-        repeats[hand[i]] = repeats[hand[i]] == null ? 1 : repeats[hand[i]]+=1
+        repeats[hand[i]] = repeats[hand[i]] == null ? 1 : repeats[hand[i]] += 1
     }
     let cardCount = Object.keys(repeats).length;
-    for(const [key, value] of Object.entries(repeats)){
+    for (const [key, value] of Object.entries(repeats)) {
         switch (value) {
             case 5:
-                hands[hand] = 7
-                console.log("5 o kind")
+                hands[hand] = 7;
+                console.log("5 o kind");
                 break;
             case 4:
-                hands[hand] = 6
-                console.log("4 o kind")
+                hands[hand] = 6;
+                console.log("4 o kind");
                 break;
             case 3:
-                if(cardCount === 3){
-                    hands[hand] = 4
-                }else if(cardCount === 2){
-                    hands[hand] = 5
+                if (cardCount === 3) {
+                    hands[hand] = 4;
+                } else if (cardCount === 2) {
+                    hands[hand] = 5;
                 }
-                console.log("3 card")
+                console.log("3 card");
                 break;
             case 2:
-                if(cardCount === 4){
-                    hands[hand] = 1
-                }else if(cardCount === 3){
-                    hands[hand] = 2
-                }              
+                if (cardCount === 4) {
+                    hands[hand] = 1;
+                } else if (cardCount === 3) {
+                    hands[hand] = 2;
+                }
                 break;
             default:
                 break;
         }
     }
+
     //console.log();
 
 }
+let keys = Object.keys(hands);
+for (let level1 = 1; level1 < keys.length; level1++) {
+    for (let level2 = 1; level2 < keys.length; level2++) {
+        if(keys[level1] != keys[level2]){
+            for (let i = 0; i < keys[level1].length; i++) {
+                if (cards.indexOf(keys[level1][i]) !== cards.indexOf(keys[level2][i]))
+                console.log(cards.indexOf(keys[level1][i]) > cards.indexOf(keys[level2][i]) ? hands[keys[level1]] : hands[keys[level2]] )
+            }
+        }
+    }
+}
+
 console.log(hands);
