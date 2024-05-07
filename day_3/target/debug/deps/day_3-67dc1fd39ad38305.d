73e53ceb2d5fa1faf329577a7049478112c2f195
ILCart <isaac.lay23@student.cart.org>
2024-05-07 09:59:11 -0700

    day3 init

---
+++ day_3/target/debug/deps/day_3-67dc1fd39ad38305.d
@@ -0,0 +1,5 @@
+C:\Users\150073110\Documents\GitHub\adventofcode\day_3\target\debug\deps\libday_3-67dc1fd39ad38305.rmeta: src\main.rs
+
+C:\Users\150073110\Documents\GitHub\adventofcode\day_3\target\debug\deps\day_3-67dc1fd39ad38305.d: src\main.rs
+
+src\main.rs:
---
+++ day_3/target/debug/deps/day_3-d0edc4d0ad08e8bf.d
@@ -0,0 +1,5 @@
+C:\Users\150073110\Documents\GitHub\adventofcode\day_3\target\debug\deps\libday_3-d0edc4d0ad08e8bf.rmeta: src\main.rs
+
+C:\Users\150073110\Documents\GitHub\adventofcode\day_3\target\debug\deps\day_3-d0edc4d0ad08e8bf.d: src\main.rs
+
+src\main.rs:
